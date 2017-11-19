34e932e805f1fb380fda887ec73be964a1dc0e16
Marcelo Torres <marcelotores21@gmail.com>
2017-11-19 08:55:03 -0300

    primeiro commit das migrates

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+    	/*
+        User::create([
+        	'name' => 'Marcelo',
+        	'email' => 'marcelotores21@gmail.com',
+        	'username' => 'marcelotores',
+        	'password' => bcrypt('123456')
+        ]);
+
+        */
+    }
+}
