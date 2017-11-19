74d0bdd8d1a9b1dd9f2dc1b9072a4d996d7c0281
Marcelo Torres <marcelotores21@gmail.com>
2017-11-19 10:54:08 -0300

    atualizacao factory

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -9,9 +9,13 @@ class DatabaseSeeder extends Seeder
      *
      * @return void
      */
+
+
+    //Lista dos valores que serão gerados
     public function run()
     {
          //$this->call(UserTableSeeder::class);
-    	factory(App\User::class, 7)->create();
+    	factory(App\User::class, 7)->create();  //7 usuários diferentes serão gerados
+    	factory(App\Product::class, 15)->create(); //15 produtos diferentes serão gerados
     }
 }
